f973450a16530c12825c5b5aa0e09d6a031d7689
Sergey Sirotkin <niktoris0@gmail.com>
2023-12-02 23:40:28 +0300

    Добавил изменения

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,76 +1,65 @@
-﻿/ Создаем функцию по вводу размера массива
-int ReadInt(string text)
-    {
-        System.Console.Write(text);
-        return Convert.ToInt32(Console.ReadLine());
-    }
-// Создаем функцию по заполнению массива
-int[] GenerateArray(int size, int leftRange, int rightRange)
-    {
-        int[] tempArray = new int[size];
-        Random rand = new Random();
-
-        for (int i = 0; i < size; i++)
-    {
-        tempArray[i] = rand.Next(leftRange, rightRange + 1);
-    }
-
-    return tempArray;
-    }
-// Создаем функцию по выводу массива
-void PrintArray(int[] array)
-    {
-        System.Console.WriteLine($"[{string.Join(", ", array)}]");
-    }
-// создаем функцию по нахождению количества чисел, которое заканчивается на 1 и делится на 7
-int CountDivBy7(int[] array)
-{
-    int count = 0;
-// foreach (int number in array) 
-//     if (number % 7 == 0 && number % 10 ==1) {
-//         count++;
-//     }
-    for (int number =0; number < array.Length; number++)
-    {
-        if (array[number] % 7 == 0 && array[number] % 10 ==1){
-            count++;
-    }
-    }
-            return count;
-}
-// Основная программа
-
-int size = ReadInt("Введите размер массива: ");
-int[] myArray = GenerateArray(size, 0, 100);
-myArray[1] = 21;
-PrintArray(myArray);
-
-int count = CountDivBy7(myArray);
-
-Console.WriteLine($"Количество чисел, которые оканчиваются на 1 и делятся нацело на 7 = {count}");
-
+﻿
 // Задача 1: Напишите программу, которая бесконечно
 // запрашивает целые числа с консоли. Программа
 // завершается при вводе символа ‘q’ или при вводе
 // числа, сумма цифр которого четная.
 // 5 12 16 q [STOP]
 // 3 45 342 15 [STOP]
+
+// Функция проверки на четность суммы цифр числа
+int CheckN(int N)
+{
+    int k = 0;
+    int sum = 0;
+    while (N > 0)
+    {
+        int number = N % 10;
+        sum = sum + number;
+        N/=10;
+    }
+    if (sum % 2 == 0)
+        k = 1;
+    return k;
+}
+// Решение задачи
+
+int i = 0; 
+while ( i < 1)
+{
+    Console.Write("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int s1 = Console.Read();
+    if (s1 ==113)
+    {
+    System.Console.Write("[STOP]");
+    break;
+    }
+    i = CheckN(N);
+}
+System.Console.Write("[STOP]");
+
+
+
+
+
+
+
 // Создаем функцию по вводу размера массива
-int ReadInt(string text)
-    {
-        System.Console.Write(text);
-        return Convert.ToInt32(Console.ReadLine());
-    }
+// int ReadInt(string text)
+//     {
+    //     System.Console.Write(text);
+    //     return Convert.ToInt32(Console.ReadLine());
+    // }
 
-    int[] GenerateArray(int size, int leftRange, int rightRange)
-    {
-        int[] tempArray = new int[size];
-        Random rand = new Random();
+    // int[] GenerateArray(int size, int leftRange, int rightRange)
+    // {
+    //     int[] tempArray = new int[size];
+    //     Random rand = new Random();
 
-        for (int i = 0; i < size; i++)
-    {
-        tempArray[i] = rand.Next(leftRange, rightRange + 1);
-    }
+    //     for (int i = 0; i < size; i++)
+    // {
+    //     tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    // }
 
-    return tempArray;
-    }
+    // return tempArray;
+    // }
