316e5daea1633f0b936908b6eb84c5116c5fc428
Sergey Sirotkin <niktoris0@gmail.com>
2023-12-03 18:25:09 +0300

    Добавил решение

--- task001/Program.cs
+++ task001/Program.cs
@@ -18,7 +18,7 @@ int CheckN(int N)
         N/=10;
     }
     if (sum % 2 == 0)
-        k = 1;
+    k = 1;
     return k;
 }
 // Решение задачи
@@ -27,39 +27,15 @@ int i = 0;
 while ( i < 1)
 {
     Console.Write("Введите число: ");
-    int N = Convert.ToInt32(Console.ReadLine());
-    int s1 = Console.Read();
-    if (s1 ==113)
+    int N;
+    string sq = Console.ReadLine();
+    if (int.TryParse(sq, out N))
     {
-    System.Console.Write("[STOP]");
-    break;
-    }
     i = CheckN(N);
+    }
+    else
+    {
+        i = 1;
+    }
 }
 System.Console.Write("[STOP]");
-
-
-
-
-
-
-
-// Создаем функцию по вводу размера массива
-// int ReadInt(string text)
-//     {
-    //     System.Console.Write(text);
-    //     return Convert.ToInt32(Console.ReadLine());
-    // }
-
-    // int[] GenerateArray(int size, int leftRange, int rightRange)
-    // {
-    //     int[] tempArray = new int[size];
-    //     Random rand = new Random();
-
-    //     for (int i = 0; i < size; i++)
-    // {
-    //     tempArray[i] = rand.Next(leftRange, rightRange + 1);
-    // }
-
-    // return tempArray;
-    // }
